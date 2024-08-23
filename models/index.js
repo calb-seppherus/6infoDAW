df3ce5dc98c65770e7fcda49d7d4afe372c97738
lucas <gabriel.freitas2006@proton.me>
2024-08-23 11:27:12 -0300

    adicionado comentario

--- models/index.js
+++ models/index.js
@@ -20,12 +20,15 @@ db.lojas = require("./loja.model.js")(sequelize, Sequelize);
 db.usuario = require("./usuario.model.js")(sequelize, Sequelize);
 db.categoria = require("./categoria.model.js")(sequelize, Sequelize);
 
+//Relacionamento 1:1
 db.usuario.hasOne(db.lojas);
 db.lojas.belongsTo(db.usuario);
 
+//Relacionamento 1:*
 db.lojas.hasMany(db.produtos);
 db.produtos.belongsTo(db.lojas);
 
+//Relacionamento */*
 db.produtos.belongsToMany(db.categoria, {through:"produtos_categoria"});
 db.categoria.belongsToMany(db.produtos, {through:"produtos_categoria"});
 
