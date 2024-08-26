5270afc8ab2c1c622f3265f71c2184269c72b8f2
lucas <gabriel.freitas2006@proton.me>
2024-08-26 12:20:42 -0300

    adicionado controller para produto, construido os metodos de produto

---
+++ controllers/produto.controller.js
@@ -0,0 +1,102 @@
+const db = require("../models");
+const Produto = db.produto;
+const Op = db.Sequelize.Op;
+
+exports.create = (req, res) => {
+
+    if (!req.body.nome) {
+        res.status (400) .send ({
+            message: "Conteudo não pode estar vazio!",
+        });
+        return;
+    }
+
+
+    const produto = {
+        nome: req.body.nome,
+        descricao: req.body.descricao,
+        preco: req.body.preco,
+        foto: req.body.foto,
+        lojaId: req.body.lojaId,
+    };
+
+
+    produto.create(produto)
+        .then((data) => {
+            res.send(data);
+        })
+        
+        .catch((err) => {
+            res.status (500).send({
+                message: err.message || "Erro durante a criação do produto",
+            });
+        });
+};
+
+exports.findAll = (req, res) => {
+    const nome = req.query.nome;
+        var condition = nome ? { nome: { [Op.iLike]: `%${nome}%` } } : null;
+
+    Produto.findAll({ where: condition })
+        .then((data) => {
+            res.send(data);
+        })
+        
+        .catch((err) => {
+            res.status (500).send({
+                message: err.message || "Erro durante a procura do produto",
+            });
+        });
+};
+
+exports.findOne = (req, res) => {
+        const id = req.params.id;
+
+    Produto.findByPk(id)
+    .then((data) => {
+        if (data) {
+            res.send(data);
+        } else {
+            res.status (404).send({
+                message: `Não é possivel achar o Produto com o id= ${id}.`,
+            });
+        }
+    })
+    .catch((err) => {
+        res.status (500).send({
+            message: "Erro na busca por produto pelo id=" + id,
+        });
+    });
+};
+
+exports.update = (req, res) => {
+    const id = req.params.id;
+
+   Produto.update (req.body, {
+    where: { id: id },
+   })
+        .then((num) => {
+            if (num == 1) {
+                res.send({
+                    message: "produto foi atualizado com sucesso.",
+                });
+            } else {
+                res.send({
+                    message: `Não foi possível atualizar Produto com id= ${id}. Talvez o 
+                    produto não tenha sido encontrada ou req.body está vazio!` ,
+                });
+            }
+        })
+        .catch((err) => {
+            res.status (500).send ({
+                message : "Erro em atualizar a Produto via id=" + id,
+            });
+        });
+};
+
+exports.delete = (req, res) => {
+
+};
+
+exports.deleteAll = (req, res) => {
+};
