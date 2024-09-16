df83d336267c2e619e1f1156a7bb7e3cd2dfc693
lucas <gabriel.freitas2006@proton.me>
2024-09-16 11:54:58 -0300

    feito as rotas para post e get de imagens

--- routes/loja.routes.js
+++ routes/loja.routes.js
@@ -1,6 +1,41 @@
 module.exports = (app) => {
-    const lojas = require("../controllers/loja.controller");
+
+    const multer = require("multer");
+    const fs = require("fs");
+    var path = require("path");
     var router = require("express").Router();
+    
+
+    var storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, "uploads/loja");
+        },
+        filename: function(req, file, cb) {
+            cb(null, Date.now() + path.extname(file.originalname));
+        }
+    });
+
+    const upload = multer({
+        storage: storage,
+    });
+
+    router.post("/upload/", upload.single("file"), async (req,res) => {
+        res.send({
+            upload: true,
+            file: req.file
+        });
+    });
+
+    router.get("/upload/:arquivo", (req,res) => {
+        const arquivo = path.dirname(__dirname)
+        + `/uploads/loja/${req.params.arquivo}`;
+        fs.readFile(arquivo, function (err, data) {
+            res.contentType("png");
+            res.send(data);
+        });
+    });
+
+    const lojas = require("../controllers/loja.controller");
 
     // Rota que cria uma loja
     router.post("/", lojas.create);
